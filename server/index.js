1df323f2970d13c762413d02f1b30f7446384234
nishantvekariya1 <nishantvekariya623@gmail.com>
2024-03-10 15:08:58 +0530

    paytm payment gateway

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import Connection from "./database/db.js";
 import DefaultData from "./default.js";
 import Router from "./routes/route.js";
+import { v4 as uuid } from "uuid";
 
 const app = express();
 
@@ -28,3 +29,16 @@ app.listen(PORT, () => {
 });
 
 DefaultData();
+
+export let paytmMerchantkey = process.env.PAYTM_MERCHANT_KEY;
+export let paytmParams = {};
+(paytmParams["MID"] = process.env.PAYTM_MID),
+  (paytmParams["WEBSITE"] = process.env.PAYTM_WEBSITE),
+  (paytmParams["CHANNEL_ID"] = process.env.PAYTM_CHANNEL_ID),
+  (paytmParams["INDUSTRY_TYPE_ID"] = process.env.PAYTM_INDUSTRY_TYPE_ID),
+  (paytmParams["ORDER_ID"] = uuid()),
+  (paytmParams["CUST_ID"] = process.env.PAYTM_CUST_ID),
+  (paytmParams["TXN_AMOUNT"] = "100"),
+  (paytmParams["CALLBACK_URL"] = "http://localhost:8000/callback");
+paytmParams["EMAIL"] = "vekariyanishant1@gmail.com";
+paytmParams["MOBILE_NO"] = "1234567852";
--- server/routes/route.js
+++ server/routes/route.js
@@ -4,6 +4,10 @@ import {
   getProducts,
   getProductById,
 } from "../controller/product-controller.js";
+import {
+  addPaymentGateway,
+  paymentResponse,
+} from "../controller/payment-controller.js";
 
 const router = express.Router();
 
@@ -12,5 +16,8 @@ router.post("/login", userLogin);
 
 router.get("/products", getProducts);
 router.get("/product/:id", getProductById);
+
+router.post("/payment", addPaymentGateway);
+router.post("/callback", paymentResponse);
 
 export default router;
